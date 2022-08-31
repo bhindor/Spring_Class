3c37f419afa1829cd144d5617cd63199237ccfd6
bhindor <82446373+bhindor@users.noreply.github.com>
2022-08-31 14:29:03 +0900

    ex01
    2022-08-31

---
+++ ex01_springboot/ex01_springboot/src/main/java/com/example/ex01_springboot/Ex01SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ex01_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex01SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex01SpringbootApplication.class, args);
+	}
+
+}
